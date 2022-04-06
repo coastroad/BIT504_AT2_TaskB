4f30b3db3b5fab42340d18d2447089e6900f5868
coastroad <coastroadhotsauce@gmail.com>
2022-04-06 19:36:10 +1200

    set up constructor in Pong class

--- Pong/src/PongPackage/Pong.java
+++ Pong/src/PongPackage/Pong.java
@@ -6,9 +6,18 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame {
 
+	//constructor
+	public Pong() {
+		setTitle("Pong");
+		setSize(800, 600);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test");
+		new Pong();
 	}
 
 }
