d44321f1f45ebba3ecbb5cd741982d5f4a8b3dcf
coastroad <coastroadhotsauce@gmail.com>
2022-04-06 20:00:07 +1200

    Changed Pong variables to use final static variables.

--- Pong/src/PongPackage/Pong.java
+++ Pong/src/PongPackage/Pong.java
@@ -6,10 +6,17 @@ import javax.swing.JFrame;
 
 public class Pong extends JFrame {
 
+	//variables
+	
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	
+	
 	//constructor
 	public Pong() {
-		setTitle("Pong");
-		setSize(800, 600);
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
