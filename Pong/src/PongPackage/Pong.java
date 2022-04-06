d2369aadf88f183a14420183fe66f20248d45248
coastroad <coastroadhotsauce@gmail.com>
2022-04-06 19:30:14 +1200

    Ignore serialised error warnings

--- Pong/src/PongPackage/Pong.java
+++ Pong/src/PongPackage/Pong.java
@@ -1,6 +1,10 @@
+
 package PongPackage;
 
-public class Pong {
+import javax.swing.JFrame;
+
+
+public class Pong extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- Pong/src/module-info.java
+++ Pong/src/module-info.java
@@ -1,2 +1,3 @@
 module Pong {
+	requires java.desktop;
 }
