64064f4fa866871bc4fedf3993a984e86724e450
coastroad <coastroadhotsauce@gmail.com>
2022-04-10 15:34:32 +1200

    Created ball class

---
+++ Pong/src/PongPackage/Ball.java
@@ -0,0 +1,22 @@
+package PongPackage;
+
+import java.awt.Color;
+
+public class Ball extends Sprite {
+
+	//static variable
+	private final static Color BALL_COLOUR = Color.WHITE;
+	private final static int BALL_WIDTH = 25;
+	private final static int BALL_HEIGHT = 25;
+	
+	
+	//constructor
+	public Ball(int panelWidth, int panelHeight) {
+		
+		setWidth(BALL_WIDTH);
+		setHeight(BALL_HEIGHT);
+		setColour(BALL_COLOUR);
+		setInitialPosition(panelWidth / 2 - (getWidth() / 2), panelHeight / 2 - (getHeight() / 2));
+		resetToInitialPosition();
+	}
+}
