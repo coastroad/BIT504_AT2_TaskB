b6ff2a657f37edf83b1a1b0de309aa6e47d1b740
coastroad <coastroadhotsauce@gmail.com>
2022-04-10 13:53:46 +1200

    Added timer and paint methods to PongPanel

--- Pong/src/PongPackage/PongPanel.java
+++ Pong/src/PongPackage/PongPanel.java
@@ -1,18 +1,21 @@
 package PongPackage;
 
 import javax.swing.JPanel;
-
+import javax.swing.Timer;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
 	//variables
 	
-	private static final Color BACKGROUND_COLOUR =  Color.BLACK;
+	private final static Color BACKGROUND_COLOUR =  Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	
 	//constructor
 	public PongPanel() {
@@ -20,8 +23,19 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		
 		//sets the JPanel background colour to black
 		setBackground(BACKGROUND_COLOUR);
+		
+		//Timer for game loop
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
+	}//end of paintComponent
 
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -44,6 +58,16 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+		//calls update()
+		update();
+		
+		//calls repaint()
+		repaint();
+		
 	}
+	
+	private void update() {
+		
+	}//end of update
 
 }
